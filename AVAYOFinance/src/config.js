bcacc85edee04d56f9aa39e5373cae4e41c09df7
ivanabi56 <98284186+ivanabi56@users.noreply.github.com>
2022-04-12 14:03:11 +0200

    Update config.js

--- AVAYOFinance/src/config.js
+++ AVAYOFinance/src/config.js
@@ -2,5 +2,5 @@ export const config = {
   chainId: 56,
   contractAddress: "0xD11ea22d52d2a1374C6777bCE78d1329f6D6563c",
   scanLink:
-    "https://bscscan.com/address/0xD11ea22d52d2a1374C6777bCE78d1329f6D6563c#code",
+    "https://bscscan.com/address/0xD11ea22d52d2a1374C6777bCE78d1329f6D6563c",
 };
